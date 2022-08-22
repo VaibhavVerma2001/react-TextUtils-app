3d8a1df7ea9073bdd006124f732801f8046acb00
Vaibhav <vaibhavverma21122001@gmai.com>
2022-08-22 20:02:20 +0530

    upto props , propstypes,components

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
       {/* now we need to give values otherwise nothing will be printed if we dont use default props */}
       {/* <Navbar brand="TextUtils" about ="About TextUtils"/>  */}
       {/* <Navbar brand={3} about ="About TextUtils"/>  */} {/* warning because we set proptype as string*/}
-      <Navbar brand="TextUtils" />
+      < Navbar brand="TextUtils" />
     </>
   );
 }
